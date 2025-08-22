71f7e0b9f6c291139ca7ab4df93fc0212fda9c99
Bhavesh Patil <patilbhavesh781@gmail.com>
2025-08-22 22:56:37 +0530

    Update: merged remote changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { CartProvider } from "./context/CartContext";
@@ -10,11 +9,11 @@ import { AuthProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
-  <HashRouter>
    <AuthProvider>
   <CartProvider>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
   </CartProvider>
   </AuthProvider>
-  </HashRouter>
 );
